55664010b34a17cea11ae959f69b4f55f6f7faa8
SANDIP CHITALE <sandipchitale@gmail.com>
2023-01-28 00:10:37 -0800

    Remove first level.

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ const YAML = require('json-to-pretty-yaml');
 
 (async () => {
     const diffUsage = `
-diff-releases (v0.0.21)
+diff-releases (v0.0.22)
 
 The Kubernetes package manager custome commands:
 
